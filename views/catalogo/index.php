a39c9f6ca41a12abfc0a5fe13545f20bf49dc4da
Oscar Megía López <pfgytk@gmail.com>
2020-02-10 11:49:16 +0100

    Paso 12: Creado CRUD de la tabla catalogo

---
+++ views/catalogo/index.php
@@ -0,0 +1,35 @@
+<?php
+
+use yii\helpers\Html;
+use yii\grid\GridView;
+
+/* @var $this yii\web\View */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = 'Catalogos';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="catalogo-index">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>
+        <?= Html::a('Create Catalogo', ['create'], ['class' => 'btn btn-success']) ?>
+    </p>
+
+
+    <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        'columns' => [
+            ['class' => 'yii\grid\SerialColumn'],
+
+            'id',
+            'nombre',
+            'description',
+
+            ['class' => 'yii\grid\ActionColumn'],
+        ],
+    ]); ?>
+
+
+</div>
